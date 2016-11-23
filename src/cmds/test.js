b54a3313db864be681c554272a135f28dc851242
Alan Plum <me@pluma.io>
2016-11-23 17:08:23 +0100

    Handle ArangoError in tests
    See #1.

--- src/cmds/test.js
+++ src/cmds/test.js
@@ -1,10 +1,11 @@
 import {white, bold} from 'chalk'
 import {common} from '../util/cli'
 import client from '../util/client'
-import {fatal} from '../util/log'
+import {error, fatal} from '../util/log'
 import {group, inline as il} from '../util/text'
 import * as reporters from '../reporters'
 import resolveMount from '../resolveMount'
+import * as errors from '../errors'
 
 export const command = 'test <mount-path>'
 export const description = 'Run the tests of a mounted service'
@@ -80,6 +81,7 @@ async function runTests (db, mount, {reporter: cliReporter, raw}) {
   if (cliReporter === 'spec') cliReporter = 'suite'
   else if (cliReporter === 'json') cliReporter = 'default'
   let apiReporter = cliReporter
+
   if (
     cliReporter === 'list' ||
     cliReporter === 'min' ||
@@ -88,43 +90,75 @@ async function runTests (db, mount, {reporter: cliReporter, raw}) {
   ) {
     apiReporter = 'default'
   }
-  const start = new Date().toISOString()
-  const result = await db.runServiceTests(mount, {reporter: apiReporter})
-  if (raw || cliReporter === 'default') {
-    console.log(JSON.stringify(result, null, 2))
-    switch (apiReporter) {
+
+  try {
+    const start = new Date().toISOString()
+    const result = await db.runServiceTests(mount, {reporter: apiReporter})
+
+    if (raw || cliReporter === 'default') {
+      console.log(JSON.stringify(result, null, 2))
+      switch (apiReporter) {
+        case 'suite':
+        case 'default':
+          process.exit(result.stats.failures ? 1 : 0)
+          break
+        case 'stream':
+          process.exit(result[result.length - 1][1].failures ? 1 : 0)
+          break
+        default:
+          throw new Error(`Unknown reporter type "${white(apiReporter)}".`)
+      }
+    }
+    switch (cliReporter) {
+      case 'list':
+      case 'min':
+        reporters.list(result, cliReporter === 'min')
+        break
       case 'suite':
-      case 'default':
-        process.exit(result.stats.failures ? 1 : 0)
+        reporters.suite(result)
         break
       case 'stream':
-        process.exit(result[result.length - 1][1].failures ? 1 : 0)
+        reporters.stream(result)
+        break
+      case 'tap':
+        reporters.tap(result)
+        break
+      case 'xunit':
+        const hostname = db._connection._baseUrl.hostname
+        reporters.xunit(result, {start, mount, hostname})
         break
       default:
         throw new Error(`Unknown reporter type "${white(apiReporter)}".`)
     }
-  }
-  switch (cliReporter) {
-    case 'list':
-    case 'min':
-      reporters.list(result, cliReporter === 'min')
-      break
-    case 'suite':
-      reporters.suite(result)
-      break
-    case 'stream':
-      reporters.stream(result)
-      break
-    case 'tap':
-      reporters.tap(result)
-      break
-    case 'xunit':
-      const hostname = db._connection._baseUrl.hostname
-      reporters.xunit(result, {start, mount, hostname})
-      break
-    default:
-      throw new Error(`Unknown reporter type "${white(apiReporter)}".`)
+  } catch (e) {
+    if (e.isArangoError) {
+      switch (e.errorNum) {
+        case errors.ERROR_SERVICE_NOT_FOUND:
+          error(`No service found at "${white(mount)}".`)
+          process.exit(1)
+          break
+        case errors.ERROR_SERVICE_NEEDS_CONFIGURATION:
+          error(`Service at "${white(mount)}" is missing configuration or dependencies.`)
+          process.exit(1)
+          break
+        case errors.ERROR_MODULE_NOT_FOUND:
+          error('An error occured while trying to execute the tests:')
+          error(e)
+          error('This typically means the tests tried to require a path that does not exist.')
+          error('Make sure the service bundle includes all the files you expect.')
+          process.exit(1)
+          break
+        case errors.ERROR_MODULE_FAILURE:
+          error('An error occured while trying to execute the tests:')
+          error(e)
+          error(il`
+            Make sure all tests are specified via the manifest
+            not loaded directly from another test file.
+          `)
+          process.exit(1)
+          break
+      }
+    }
+    throw e
   }
 }
-
-
