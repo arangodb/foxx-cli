05ad840bb183abc2d67641cb565b1d44ab89d0ef
mpv1989 <mark@arangodb.com>
2018-02-13 17:27:32 +0100

    Fix test util

--- test/util/index.js
+++ test/util/index.js
@@ -3,10 +3,15 @@
 const exec = require("child_process").execSync;
 const path = require("path");
 
-module.exports = command => {
-  return JSON.parse(
-    exec(`node foxx ${command} --raw`, {
+module.exports = (command, raw = false) => {
+  if (raw)
+    return JSON.parse(
+      exec(`node foxx ${command} --raw`, {
+        cwd: path.resolve(__dirname, "..", "..", "bin")
+      })
+    );
+  else
+    return exec(`node foxx ${command}`, {
       cwd: path.resolve(__dirname, "..", "..", "bin")
-    })
-  );
+    }).toString("utf-8");
 };
