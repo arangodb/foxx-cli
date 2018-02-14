7a8b5f7a481a798806f5d7d97148efd34545ba28
mpv1989 <mark@arangodb.com>
2018-02-14 11:11:52 +0100

    Add list tests

--- test/list.js
+++ test/list.js
@@ -1,13 +1,44 @@
 /* global describe, it */
 "use strict";
 
+const path = require("path");
 const foxx = require("./util");
 const expect = require("chai").expect;
+
+const basePath = path.resolve(".", "test", "fixtures");
 
 describe("Foxx service list", () => {
   it("should exclude system services", () => {
     const services = foxx("list", true);
     expect(services).to.be.instanceOf(Array);
     expect(services.length).to.equal(0);
+  });
+
+  it("should include installed service", async () => {
+    const mount = "/list-test";
+    try {
+      foxx(
+        `install ${mount} ${path.resolve(
+          basePath,
+          "minimal-working-service.zip"
+        )}`
+      );
+      const services = foxx("list", true);
+      expect(services).to.be.instanceOf(Array);
+      expect(services.length).to.equal(1);
+      const service = services.find(service => service.mount === mount);
+      expect(service).to.have.property("name", "minimal-working-manifest");
+      expect(service).to.have.property("version", "0.0.0");
+      expect(service).to.have.property("provides");
+      expect(service.provides).to.eql({});
+      expect(service).to.have.property("development", false);
+      expect(service).to.have.property("legacy", false);
+    } finally {
+      try {
+        foxx(`uninstall ${mount}`);
+      } catch (e) {
+        // noop
+      }
+    }
   });
 });
