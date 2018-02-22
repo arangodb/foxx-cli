9d26c7b29a0eee1caf9d98eb4092871d4fd81011
mpv1989 <mark@arangodb.com>
2018-02-22 15:02:33 +0100

    Fix test timeouts

--- test/util/index.js
+++ test/util/index.js
@@ -4,14 +4,10 @@ const exec = require("child_process").execSync;
 const path = require("path");
 
 module.exports = (command, raw = false) => {
-  if (raw)
-    return JSON.parse(
-      exec(`node foxx ${command} --raw`, {
-        cwd: path.resolve(__dirname, "..", "..", "bin")
-      })
-    );
-  else
-    return exec(`node foxx ${command}`, {
-      cwd: path.resolve(__dirname, "..", "..", "bin")
-    }).toString("utf-8");
+  const options = {
+    cwd: path.resolve(__dirname, "..", "..", "bin"),
+    timeout: 5000
+  };
+  if (raw) return JSON.parse(exec(`node foxx ${command} --raw`, options));
+  else return exec(`node foxx ${command}`, options).toString("utf-8");
 };
