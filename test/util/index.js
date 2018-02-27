e456bed49cec9522de73aa4a75f0d28c2abfdba3
mpv1989 <mark@arangodb.com>
2018-02-27 10:20:15 +0100

    Enhance test util

--- test/util/index.js
+++ test/util/index.js
@@ -3,10 +3,8 @@
 const exec = require("child_process").execSync;
 const path = require("path");
 
-module.exports = (command, raw = false) => {
-  const options = {
-    cwd: path.resolve(__dirname, "..", "..", "bin")
-  };
-  if (raw) return JSON.parse(exec(`node foxx ${command} --raw`, options));
-  else return exec(`node foxx ${command}`, options).toString("utf-8");
+module.exports = (command, raw = false, cwd) => {
+  const foxx = `node ${path.resolve(__dirname, "..", "..", "bin", "foxx")}`;
+  if (raw) return JSON.parse(exec(`${foxx} ${command} --raw`, { cwd }));
+  else return exec(`${foxx} ${command}`, { cwd }).toString("utf-8");
 };
