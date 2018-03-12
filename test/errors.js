d8d80d75176bdcd31f4f03c2a3b915c1a5c1ea82
Alan Plum <me@pluma.io>
2018-03-12 12:59:08 +0100

    Add tests for foxx test errors

--- test/errors.js
+++ test/errors.js
@@ -177,6 +177,43 @@ describe.only("Error handling", () => {
       }
       expect.fail();
     });
+    it("correctly handles SERVICE_NEEDS_CONFIGURATION", async () => {
+      ERROR = errors.ERROR_SERVICE_NEEDS_CONFIGURATION;
+      try {
+        await foxx(`test -H ${HOST} /myfoxx`);
+      } catch (e) {
+        const stderr = e.stderr.toString("utf-8");
+        expect(stderr).not.to.match(/unexpected/i);
+        expect(stderr).to.include("/myfoxx");
+        expect(stderr).to.match(/config/i);
+        return;
+      }
+      expect.fail();
+    });
+    it("correctly handles MODULE_NOT_FOUND", async () => {
+      ERROR = errors.ERROR_MODULE_NOT_FOUND;
+      try {
+        await foxx(`test -H ${HOST} /myfoxx`);
+      } catch (e) {
+        const stderr = e.stderr.toString("utf-8");
+        expect(stderr).not.to.match(/unexpected/i);
+        expect(stderr).to.match(/require/i);
+        return;
+      }
+      expect.fail();
+    });
+    it("correctly handles MODULE_FAILURE", async () => {
+      ERROR = errors.ERROR_MODULE_FAILURE;
+      try {
+        await foxx(`test -H ${HOST} /myfoxx`);
+      } catch (e) {
+        const stderr = e.stderr.toString("utf-8");
+        expect(stderr).not.to.match(/unexpected/i);
+        expect(stderr).to.match(/manifest/i);
+        return;
+      }
+      expect.fail();
+    });
   });
   describe("Foxx upgrade", () => {
     it("correctly handles SERVICE_NOT_FOUND", async () => {
