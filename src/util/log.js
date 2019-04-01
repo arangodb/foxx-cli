530e285d1f7bf031e39af1a3c0db7f0cd833f5ef
Alan Plum <me@pluma.io>
2019-04-01 10:43:39 +0200

    Better handling of unknown sys errors

--- src/util/log.js
+++ src/util/log.js
@@ -54,6 +54,12 @@ exports.fatal = function fatal(err) {
         err.port
       )}\nThis indicates connectivity issues or a server problem. Are you offline?`
     );
+  } else if (err.code === "EHOSTUNREACH") {
+    exports.error(
+      `Host unreachable: ${red.bold(err.address)}${
+        err.port ? `:${red.bold(err.port)}` : ""
+      }\nThis indicates connectivity issues or a server problem. Is the server accessible from this network?`
+    );
   } else if (err.code === "ECONNREFUSED") {
     exports.error(
       `Connection refused: ${red.bold(err.address)}${
@@ -78,6 +84,22 @@ exports.fatal = function fatal(err) {
     exports.error(
       `Operation timed out. Server is not responding.\nThis indicates connectivity issues or a server problem.`
     );
+  } else if (
+    typeof err.code === "string" &&
+    err.code.match(/^E[A-Z]+$/) &&
+    !err.errorNum
+  ) {
+    exports.error(
+      `Unknown system error:\n\n${bold(
+        format(err.stack || err.message || err)
+      )}\n\n\nThis may indicate connectivity issues or a server problem. See the list of error names in the errno(3) man page for more information: ${bold(
+        "http://man7.org/linux/man-pages/man3/errno.3.html"
+      )}\n\nIf you believe this to be an bug in ${bold(
+        "foxx-cli"
+      )} please open an issue at ${bold(
+        bugsUrl
+      )} with a full copy of the error message and a description of what you were trying to do when this problem occurred.`
+    );
   } else if (err.isArangoError) {
     if (err.errorNum === 11) {
       exports.error(
@@ -94,30 +116,25 @@ exports.fatal = function fatal(err) {
     exports.error("Authentication failed. Bad username or password?");
   } else if (typeof err.statusCode === "number") {
     exports.error(
-      `The server responded with a ${bold(err.statusCode)} status code.\n` +
-        (err.statusCode >= 500
-          ? "This typically indicates a server-side error.\n"
-          : "This typically indicates a problem with the request.\n") +
-        "Please check the ArangoDB log file to determine the cause of this error.\n\n" +
-        `If you believe this to be an bug in ${bold("foxx-cli")} ` +
-        `please open an issue at ${bold(
-          bugsUrl
-        )} with the relevant part of the ArangoDB log` +
-        "and a description of what you were trying to do when this problem occured.\n\n" +
-        "We apologize for the inconvenience."
+      `The server responded with a ${bold(err.statusCode)} status code.\n${
+        err.statusCode >= 500
+          ? "This typically indicates a server-side error."
+          : "This typically indicates a problem with the request."
+      }\nPlease check the ArangoDB log file to determine the cause of this error.\n\nIf you believe this to be an bug in ${bold(
+        "foxx-cli"
+      )} please open an issue at ${bold(
+        bugsUrl
+      )} with the relevant part of the ArangoDB log and a description of what you were trying to do when this problem occurred.\n\nWe apologize for the inconvenience.`
     );
   } else {
     exports.error(
       `Sorry! An unexpected error occurred. This is likely a bug in ${bold(
         "foxx-cli"
-      )}.\n` +
-        `Please open an issue at ${bold(
-          bugsUrl
-        )} with a full copy of the following error message ` +
-        "and a description of what you were trying to do when this problem occured.\n\n" +
-        bold(format(err.stack || err.message || err)) +
-        "\n\n" +
-        "We apologize for the inconvenience."
+      )}.\nPlease open an issue at ${bold(
+        bugsUrl
+      )} with a full copy of the following error message and a description of what you were trying to do when this problem occurred.\n\n${bold(
+        format(err.stack || err.message || err)
+      )}\n\nWe apologize for the inconvenience.`
     );
   }
   process.exit(1);
